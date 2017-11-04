48ffd4e4802e9125e650f646fcf13594d7110e45
Roberto Simonetti <robisim74@gmail.com>
2017-11-04 23:39:02 +0100

    Building

--- src/app/resource/resource.component.ts
+++ src/app/resource/resource.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit, ElementRef } from '@angular/core';
-import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
     selector: 'app-resource',
